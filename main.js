132272db52456a1508a6f98ddc2a2dc239e84c1d
Jacob <jperez32017@gmail.com>
2023-11-06 21:56:02 -0800

    Working on GameFlow, implemented turn-switcher.

--- main.js
+++ main.js
@@ -11,56 +11,78 @@ const Player = (name, symbol) => {
 function GameBoard() {
     const rows = 3
     const columns = 3;
-    const board = []
+    const board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push("");
-        }
+    const updateBoard = (one, two, playerSymbol) => {
+        // make index become value of Player
+        board[one][two] = playerSymbol;
+        // ** check to see if index is taken **
+    }
+    
+    const printGame = () => {
+
     }
 
-    // see if cell is empty
-    const isCellEmpty = (cellIndex) => board[cellIndex] === ' ';
-
-    const updateCell = (cellIndex, symbol) => {
-        if(isCellEmpty(cellIndex)) {
-            board[cell] = symbol;
-            return true; // move successful
-        }
-        return false; // Cell is already occupied
-    }
-
-    const getBoard = () => [...board];
-
-
+    // GameBoard Return Statement
     return {
-        getBoard,
-        updateCell,
         board,
-    };
+        updateBoard,
+    }
 }
 
-const GameFlow = ((player1, player2) => {
-    let currentPlayer = player1;
+function Flow(player1Name, player2Name) {
 
-    const switchPlayer = () => {
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-    };
+    const player1 = Player(player1Name, 'X');
+    const player2 = Player(player2Name, 'O');
+    const myBoard = GameBoard()
+    let playerTurn = player1
 
-    const makeMove = (cellIndex) => {
-        if (myBoard.updateCell(cellIndex, currentPlayer.symbol)) {
-            switchPlayer();
-            return true;
-        }
-        return false;
+    const showPlayers = () => {
+        return [player1, player2]
     }
+
+    const switchTurns = () => { // switches players turn
+        playerTurn = playerTurn === player1 ? player2 : player1;
+    }
+
+    const makeMove = (index1, index2) => {
+    
+        // should change the value of the index
+        myBoard.updateBoard(index1, index2, playerTurn.symbol);
+        // switch players turn after updating
+        switchTurns();
+    }
+
+    const checkWinner = () => {
+        
+    }
+
+
     return {
+        showPlayers,
+        myBoard,
         makeMove,
     }
-})(Player('Player 1', 'X'), Player('Player 2', '0'));
+}
 
-const myBoard = GameBoard();
+// const game = GameBoard();
+// console.log(game.board);
+// game.updateBoard(0, 0);
+// console.log(game.board);
 
-console.log(myBoard.board)
+const newGame = Flow('Jake', 'Josh')
+console.log(newGame.showPlayers())
+
+newGame.makeMove(1, 1); // X
+newGame.makeMove(2, 0); // O
+console.log(newGame.myBoard.board)
+newGame.makeMove(1, 0); // X
+newGame.makeMove(2, 1); // O
+
+console.log(newGame.myBoard.board)
+
 
