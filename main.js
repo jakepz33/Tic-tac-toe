c79b83a6876a04bc3d606b00f169f0bba9388d34
Jacob <jperez32017@gmail.com>
2023-11-07 09:14:29 -0800

    Added logic for identifying winner or draw. Switching turns. and selecting open squares

--- main.js
+++ main.js
@@ -19,8 +19,17 @@ function GameBoard() {
 
     const updateBoard = (one, two, playerSymbol) => {
         // make index become value of Player
-        board[one][two] = playerSymbol;
+       
         // ** check to see if index is taken **
+        if (one >= rows || two >= columns) {
+            return "Failure"
+        } else if (board[one][two] !== '') {
+            return "Failure"// player switch doesn't happen
+        } else {
+            board[one][two] = playerSymbol;
+            return "Success"
+        }
+        
     }
     
     const printGame = () => {
@@ -52,13 +61,61 @@ function Flow(player1Name, player2Name) {
     const makeMove = (index1, index2) => {
     
         // should change the value of the index
-        myBoard.updateBoard(index1, index2, playerTurn.symbol);
-        // switch players turn after updating
-        switchTurns();
+        const moveResult = myBoard.updateBoard(index1, index2, playerTurn.symbol);
+
+        if (moveResult === "Success") {
+            switchTurns()
+            console.log('Valid move')
+            const result = checkWinner(myBoard.board);
+            if (result) {
+                if (result === "draw") {
+                    console.log("It's a draw!");
+                } else {
+                    console.log(`${result} wins`);
+                }
+            }
+        } else {
+            console.log('Invalid move, try again.')
+        }        
     }
 
-    const checkWinner = () => {
-        
+    const checkWinner = (board) => {
+        for (let row = 0; row < 3; row++) {
+            if (board[row][0] === board[row][1] && board[row][0] == board[row][2] && board[row][0] !== '') {
+                return board[row][0];
+            }
+        }
+
+        for (let col = 0; col < 3; col++) {
+            if (board[0][col] === board[1][col] && board[0][col] === board[2][col] && board[0][col] !== '') {
+                return board[0][col]
+            }
+        }
+
+        // check for diagonals
+        if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] !== '') {
+            return board[0][0];
+        }
+        if (board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] !== '') {
+            return board[0][2];
+        }
+
+        // check for DRAW
+        let isDraw = true;
+        for (let row = 0; row < 3; row ++) {
+            for (let col = 0; col < 3; col++) {
+                if (board[row][col] === '') { // checks if all squares are filled.
+                    isDraw = false;           // changes to false -> break 
+                    break;
+                }
+            }
+        }
+        if (isDraw) { // all squares are filled, no winner
+            return 'draw';
+        }
+
+        // if no winner or draw yet
+        return null
     }
 
 
