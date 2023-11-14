2c1cbb7c025373a8ae5aa57046f2f56cab273ad4
Jacob <jperez32017@gmail.com>
2023-11-13 19:16:42 -0800

    Combining data from home screen with the gameboard and playing logic. Need to work on reset logic to be consistent with game.

--- main.js
+++ main.js
@@ -47,10 +47,15 @@ function Flow(player1Name = "Player1", player2Name = "Player2") {
     const player1 = Player(player1Name, 'X');
     const player2 = Player(player2Name, 'O');
     const myBoard = GameBoard()
-    let playerTurn = player1
+    console.log(player1, "NRUHH")
+    let playerTurn = player1;
+
+    // const startFirst = () => {
+    //     if()
+    // }
 
     //GET UPDATED PLAYER
-    const getCurrentPlayerTurn = () => playerTurn; // how to update a var and return it
+    const getCurrentPlayerTurn = () => ({...playerTurn}); // how to update a var and return it
 
     const showPlayers = () => {
         return [player1, player2]
@@ -100,21 +105,41 @@ function Flow(player1Name = "Player1", player2Name = "Player2") {
     const checkWinner = (board) => {
         for (let row = 0; row < 3; row++) {
             if (board[row][0] === board[row][1] && board[row][0] == board[row][2] && board[row][0] !== '') {
-                return board[row][0];
+                if (player1.symbol === board[row][0]) {
+                    return player1.name;
+                } else if (player2.symbol === board[row][0]) {
+                    return player2.name
+                }
+                // return board[row][0];
             }
         }
         for (let col = 0; col < 3; col++) {
             if (board[0][col] === board[1][col] && board[0][col] === board[2][col] && board[0][col] !== '') {
-                return board[0][col]
+                if(player1.symbol === board[0][col]) {
+                    return player1.name;
+                } else if(player2.symbol === board[col][0]) {
+                    return player2.name
+                }
+                // return board[0][col]
             }
         }
 
         // check for diagonals
         if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] !== '') {
-            return board[0][0];
+            if (player1.symbol === board[0][0]) {
+                return player1.name;
+            } else if (player2.symbol === board[0][0]) {
+                return player2.name
+            }
+            // return board[0][0];
         }
         if (board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] !== '') {
-            return board[0][2];
+            if (player1.symbol === board[0][2]) {
+                return player1.name;
+            } else if (player2.symbol === board[0][2]) {
+                return player2.name
+            }
+            // return board[0][2];
         }
 
         // check for DRAW
@@ -157,8 +182,11 @@ function Flow(player1Name = "Player1", player2Name = "Player2") {
 // newGame.makeMove(1, 0); // X
 // newGame.makeMove(2, 1); // O
 
-function screenController() {
+function ScreenController() {
+    // added this one below
     const game = Flow()
+    const selectedPlayers = PlayerSelection(game);
+    //////
     const gridBoxes = document.querySelectorAll('.grid-box');
     const resetGameButton = document.querySelector('.reset-button')
     const dialog = document.querySelector('dialog');
@@ -189,7 +217,7 @@ function screenController() {
                 if (checkForWinner != null && checkForWinner != "draw") {
                     console.log(checkForWinner, "is the winner!")
                     // let h1 = document.createElement('h1')
-                    h1.textContent = `Player ${checkForWinner} wins!`
+                    h1.textContent = `${checkForWinner} wins!`
                     // dialog.appendChild(h1);
                     dialog.showModal()
                 } else if (checkForWinner != null && checkForWinner === "draw") {
@@ -223,12 +251,21 @@ function screenController() {
 
 }
 
-function playerSelection() {
+function PlayerSelection(gameflow) {
     const player1switch = document.querySelector('#player1-checkbox');
     const player2switch = document.querySelector('#player2-checkbox');
+    const startButton = document.querySelector('.startButton');
 
+    let player1Value;
+    let player2Value;
+    
     let player1span = document.querySelector('#player1span');
     let player2span = document.querySelector('#player2span')
+
+    console.log("GAMEFLOW CHECKIN", gameflow.player1)
+    // let player_name = gameflow.player1.name;
+    // gameflow.player1.name = "Jake";
+    // console.log("CHECK", gameflow.player1)
 
     console.log(player2switch.checked);
     console.log(player1switch.textContent);
@@ -240,13 +277,17 @@ function playerSelection() {
     const get1Choice = () => player1choice;
     const get2Choice = () => player2choice;
 
+    // get updated Name
+    const updateName = () => player1Value;
+    const update2Name = () => player2Value;
+
     player1switch.addEventListener("change", function() { // call function to toggle switch 2
         if (player1switch.checked) {
             console.log("Switch is ON")
             player1choice = "O";
             player2choice = "X"
             player2switch.checked = true; // make player 2 X
-            console.log("get 1 choice", get1Choice())
+    
         } else {
             console.log("Switch is OFF")
             player2switch.checked = false;
@@ -275,11 +316,65 @@ function playerSelection() {
         player2span.textContent = get2Choice();
         // call function to update visual
     } )
+     startButton.addEventListener("click", () => {
+        const player1Input = document.querySelector('#name-input-player1');
+        const player2Input = document.querySelector('#name-input-player2')
+        // empty value === ""
+    
+        if (player1Input.value != "") {
+            player1Value = player1Input.value;
+            console.log("updating name:", updateName())
+            gameflow.player1.name = updateName();
+            gameflow.player1.symbol = get1Choice();
+            console.log('New Name:', gameflow.player1.name)
+            
+        }
+        if (player2Input.value != "") {
+            player2Value = player2Input.value;
+            gameflow.player2.name = update2Name();
+            gameflow.player2.symbol = get2Choice();
+            console.log('New Name:', gameflow.player2.name)
+            
 
+        }
+
+        const startScreen = document.getElementById('start-screen');
+        const gameboardContainer = document.getElementById('gameboard-container');
+        startScreen.style.display = 'none';
+        gameboardContainer.style.display = '';
+
+        gameflow.player1.symbol = get1Choice();
+        gameflow.player2.symbol = get2Choice();
+
+        console.log(get1Choice())
+        if(get1Choice() === "O") {
+            gameflow.switchTurns();
+        } else {
+            gameflow.playerTurn = gameflow.player1;
+        }
+        
+        console.log('NEW SYMBOL 1', gameflow.getCurrentPlayerTurn())
+        console.log('NEW SYMBOL 2', gameflow.player2.symbol)
+
+        // hide this display and show the gameboard
+
+        // console.log("updating name:", updateName())
+        // console.log(update2Name())
+
+
+    })
+
+    return {
+        get1Choice,
+        get2Choice,
+        updateName,
+        update2Name,
+    }
+        
 }
 
-playerSelection();
+// playerSelection();
 // const dialog = document.querySelector("dialog")
 // dialog.showModal();
 // function to add content to the screen
-screenController();
+ScreenController();
